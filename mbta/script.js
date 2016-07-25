8686b003ab02ec6847997bf6b07399423b8ef97a
Lillianna <llevonick@gmail.com>
2016-07-24 23:01:11 -0400

    Added in half of commuter rail info

--- mbta/script.js
+++ mbta/script.js
@@ -74,7 +74,7 @@ orangeStations["Wellington"] = {lat:42.401665, lng:-71.077135};
 orangeStations["Assembly"] = {lat:42.392739, lng:-71.077276};
 orangeStations["Sullivan Sqaure"] = {lat:42.384264, lng:-71.077000};
 orangeStations["Community College"] = {lat:42.373689, lng:-71.069685};
-orangeStations["North Station"] = {lat:42.366413, lng:-71.062144};
+orangeStations["North"] = {lat:42.366413, lng:-71.062144};
 orangeStations["Haymarket"] = {lat:42.362167, lng:-71.057792};
 orangeStations["State"] = {lat:42.358211, lng:-71.057857};
 orangeStations["Downtown Crossing"] = {lat:42.355518, lng:-71.060225};
@@ -145,6 +145,72 @@ function bluePolyline(map){
 }
 
 // Commuter Rail stations and polyline
+purpleStations = {}
+purpleStations["North"] = {lat:42.366413, lng:-71.062144};
+purpleStations["Porter Sqaure"] = {lat:42.3884, lng:-71.11914899999999};
+purpleStations["Belmont"] = {lat:42.395906, lng:-71.176246};
+purpleStations["Waverley"] = {lat:42.387486, lng:-71.190800};
+purpleStations["Waltham"] = {lat:42.374362, lng:-71.235577};
+purpleStations["Brandeis/Roberts"] = {lat:42.361869, lng:-71.260119};
+purpleStations["Kendal Green"] = {lat:42.378959, lng:-71.282462};
+purpleStations["Hastings"] = {lat:42.385758, lng:-71.289178};
+purpleStations["Silver Hill"] = {lat:42.395616, lng:-71.302359};
+purpleStations["Lincoln"] = {lat:42.414288, lng:-71.325388};
+purpleStations["Concord"] = {lat:42.456635, lng:-71.357478};
+purpleStations["West Concord"] = {lat:42.457079, lng:-71.392862};
+purpleStations["South Acton"] = {lat:42.460498, lng:-71.457686};
+purpleStations["Littleton/495"] = {lat:42.520624, lng:-71.502961};
+purpleStations["Ayer"] = {lat:42.558995, lng:-71.589200};
+purpleStations["Shirley"] = {lat:42.545069, lng:-71.648130};
+purpleStations["North Leominster"] = {lat:42.539070, lng:-71.739167};
+purpleStations["Fitchburg"] = {lat:42.580890, lng:-71.792758};
+purpleStations["Wachusett"] = {lat:42.558480, lng:-71.845224};
+//beginning of seg B (off of North)
+purpleStations["West Medford"] = {lat:42.421825, lng:-71.133291};
+purpleStations["Wedgemere"] = {lat:42.444662, lng:-71.140262};
+purpleStations["Winchester Center"] = {lat:42.451296, lng:-71.137832};
+purpleStations["Mishawum"] = {lat:42.502976, lng:-71.137035};
+purpleStations["Anderson/Woburn"] = {lat:42.517362, lng:-71.144218};
+purpleStations["Wilmington"] = {lat:42.546623, lng:-71.174355};
+purpleStations["North Billerica"] = {lat:42.593169, lng:-71.281134};
+purpleStations["Lowell"] = {lat:42.637100, lng:-71.315359};
+//beginning of seg C (off of North)
+purpleStations["Malden Center"] = {lat:42.426847, lng:-71.074291};
+purpleStations["Wyoming Hill"] = {lat:42.451719, lng:-71.069379};
+purpleStations["Melrose/Cedar Park"] = {lat:42.458728, lng:-71.069864};
+purpleStations["Greenwood"] = {lat:42.483001, lng:-71.067248};
+purpleStations["Wakefield"] = {lat:42.502180, lng:-71.075612};
+purpleStations["Reading"] = {lat:42.521604, lng:-71.107477};
+purpleStations["North Wilmington"] = {lat:42.569757, lng:-71.159791};
+purpleStations["Ballardvale"] = {lat:42.627420, lng:-71.160029};
+purpleStations["Andover"] = {lat:42.657930, lng:-71.144719};
+purpleStations["Lawrence"] = {lat:42.701831, lng:-71.152251};
+purpleStations["Bradford"] = {lat:42.766971, lng:-71.088404};
+purpleStations["Haverhill"] = {lat:42.773438, lng:-71.086504};
+//beginning of seg D (off of North)
+purpleStations["Chelsea"] = {lat:42.395672, lng:-71.034306};
+purpleStations["River Works"] = {lat:42.449945, lng:-70.969854};
+purpleStations["Lynn"] = {lat:42.462381, lng:-70.945693};
+purpleStations["Swampscott"] = {lat:42.473724, lng:-70.922580};
+purpleStations["Salem"] = {lat:42.524761, lng:-70.896316};
+purpleStations["Beverly"] = {lat:42.547276, lng:-70.885458};
+purpleStations["Montserrat"] = {lat:42.562182, lng:-70.869225};
+purpleStations["Prides Crossing"] = {lat:42.559447, lng:-70.825602};
+purpleStations["Beverly Farms"] = {lat:42.561676, lng:-70.811375};
+purpleStations["Manchester"] = {lat:42.573718, lng:-70.770112};
+purpleStations["West Gloucester"] = {lat:42.611946, lng:-70.705396};
+purpleStations["Gloucester"] = {lat:42.616810, lng:-70.668360};
+purpleStations["Rockport"] = {lat:42.655485, lng:-70.627075};
+//beginning of seg E (off of Beverly)
+purpleStations["North Beverly"] = {lat:42.583774, lng:-70.883873};
+purpleStations["Hamilton/Wenham"] = {lat:42.609300, lng:-70.874818};
+purpleStations["Ipswich"] = {lat:42.676981, lng:-70.840571};
+purpleStations["Rowley"] = {lat:42.726938, lng:-70.859111};
+purpleStations["Newburyport"] = {lat:42.797263, lng:-70.877693};
+
+
+
+
 
 // Creating the map
 request = new XMLHttpRequest();
