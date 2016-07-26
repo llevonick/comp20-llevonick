144bf3f0d99e886de35e88c1ba10855fb24dd1c9
Lillianna <llevonick@gmail.com>
2016-07-25 21:52:33 -0400

    Got the messages to appear on the webpage

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,25 @@
-// Your JavaScript goes here...
+request = new XMLHttpRequest();
+
+function parse(){
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = function(){
+		if (request.readyState == 4 && request.status == 200){
+			data = request.responseText;
+			parsed = JSON.parse(data);
+
+			for(var i = 0; i < parsed.length; i++){
+				document.getElementById("messages").innerHTML += '<p class="msg">' + parsed[i]["content"]+ " " + parsed[i]["username"];
+			}
+
+		}
+		else if(request.readyState == 4 && request.status != 200){
+			document.getElementById("messages").innerHTML = "<p>Sorry something went wrong.</p>"
+		}
+		else{
+			console.log("In progress...")
+		}
+	};
+
+	request.send(null);
+}
